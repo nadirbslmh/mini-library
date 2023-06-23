281ebac66ef3bcd242dfcd991699cab12f348ead
nadirbasalamah <nadir008basalamah@gmail.com>
2023-06-23 07:08:48 +0700

    feat(ingress): add ingress configuration

--- push_image.sh
+++ push_image.sh
@@ -1,29 +1,32 @@
 # library service
 docker build -t library-service:v1 -f library.dockerfile .
-docker tag library-service:v1 nadirbasalamah/library-service:v1
-docker push nadirbasalamah/library-service:v1
+docker tag library-service:v1 dockerhub_username/library-service:v1
+docker push dockerhub_username/library-service:v1
 
 # auth
 docker build -t auth-service:v2 -f auth.dockerfile .
-docker tag auth-service:v2 nadirbasalamah/auth-service:v2
-docker push nadirbasalamah/auth-service:v2
+docker tag auth-service:v2 dockerhub_username/auth-service:v2
+docker push dockerhub_username/auth-service:v2
 
 # book
 docker build -t book-service:v1 -f book.dockerfile .
-docker tag book-service:v1 nadirbasalamah/book-service:v1
-docker push nadirbasalamah/book-service:v1
+docker tag book-service:v1 dockerhub_username/book-service:v1
+docker push dockerhub_username/book-service:v1
 
 # rent
 docker build -t rent-service:v1 -f rent.dockerfile .
-docker tag rent-service:v1 nadirbasalamah/rent-service:v1
-docker push nadirbasalamah/rent-service:v1
+docker tag rent-service:v1 dockerhub_username/rent-service:v1
+docker push dockerhub_username/rent-service:v1
 
 # logging
 docker build -t logging-service:v1 -f logging.dockerfile .
-docker tag logging-service:v1 nadirbasalamah/logging-service:v1
-docker push nadirbasalamah/logging-service:v1
+docker tag logging-service:v1 dockerhub_username/logging-service:v1
+docker push dockerhub_username/logging-service:v1
 
 # listener
 docker build -t listener-service:v1 -f listener.dockerfile .
-docker tag listener-service:v1 nadirbasalamah/listener-service:v1
-docker push nadirbasalamah/listener-service:v1
+docker tag listener-service:v1 dockerhub_username/listener-service:v1
+docker push dockerhub_username/listener-service:v1
+
+# cmd references
+minikube addons enable ingress
